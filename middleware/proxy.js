3a2c7422a5db6c3ddb15c3f0140eccee30fbb2f3
codewithpassion <dominik.fretz@gmail.com>
2014-09-08 20:03:47 -0700

    Cleanup

--- middleware/proxy.js
+++ middleware/proxy.js
@@ -15,63 +15,60 @@ bs.on('connection', function(client){
   client.on('stream', function(stream, meta) {
     var requestData = JSON.parse(meta);
 
-    if (requestData.ssl) {
-      var requestUrl = requestData.url;
-      var srvUrl = url.parse(requestUrl );
-
-      var client = net.connect({port: srvUrl.port, host: srvUrl.hostname},
-        function() { });
-      client.on('connect', function(){
-        console.log('client connected');
-        stream.write('HTTP/1.1 200 Connection Established\r\n' +
-          'Proxy-agent: Node-Proxy\r\n' +
-          '\r\n');
-      });
-
-      stream.pipe(client);
-      client.pipe(stream);
-
-      client.on('end', function() {
-        console.log('client disconnected');
-        stream.end();
-      });
-    }
-    else {
-      console.log('Stream requested, url: ' + requestData.url);
-      // we first make a HEAD request to see if the file is there. If not, or there
-      // is any other issue, we return the error to the requestor as a JSON object.
-      request.head(requestData.url, function (error, response, body) {
-        if (error || (response !== undefined && response.statusCode >= 400)) {
-          if (response) {
-            console.log(response.statusCode);
-          }
-          var statusCode = 0;
-          if (response) {
-            statusCode = response.statusCode;
-          }
-
-          stream.write(JSON.stringify({ error: error, statusCode: statusCode}));
-          stream.end();
-          console.log('There was an error: ' + error + '\nStatus Code: ' + statusCode);
-        }
-        else {
-
-          // once we are sure all is good, we go ahead and request the file and pipe it to the requestor
-          request(requestData.url, function (error, response, body) {
-            console.log('Done');
-            stream.end();
-          })
-            // this is where some magic happens.
-            // we pipe the data from the 'request()' directly to the stream
-            // on the browser, this will be piped directly to the ROV.
-            .pipe(stream);
-        }
-      });
-    }
+    var handle = requestData.ssl ? handleSsl : handleHttp;
+    handle(requestData, stream);
   });
-
 });
 server.listen(3001, function() {
   console.log('HTTP and BinaryJS server started on port 3001');
 });
 
+function handleHttp(requestData, stream) {
+  console.log('Stream requested, url: ' + requestData.url);
+  // we first make a HEAD request to see if the file is there. If not, or there
+  // is any other issue, we return the error to the requestor as a JSON object.
+  request.head(requestData.url, function (error, response, body) {
+    if (error || (response !== undefined && response.statusCode >= 400)) {
+      var statusCode = 0;
+      if (response) {
+        statusCode = response.statusCode;
+      }
+      stream.write(JSON.stringify({ error: error, statusCode: statusCode}));
+      stream.end();
+      console.log('There was an error: ' + error + '\nStatus Code: ' + statusCode);
+    }
+    else {
+
+      // once we are sure all is good, we go ahead and request the file and pipe it to the requestor
+      request(requestData.url, function (error, response, body) {
+        console.log('Done');
+        stream.end();
+      })
+        // this is where some magic happens.
+        // we pipe the data from the 'request()' directly to the stream
+        // on the browser, this will be piped directly to the ROV.
+        .pipe(stream);
+    }
+  });
+}
+
+function handleSsl(requestData, stream) {
+  var requestUrl = requestData.url;
+  var srvUrl = url.parse(requestUrl );
+
+  var client = net.connect(
+    {port: srvUrl.port, host: srvUrl.hostname},
+    function() {
+      stream.write('HTTP/1.1 200 Connection Established\r\n' +
+        'Proxy-agent: Node-Proxy\r\n' +
+        '\r\n');
+    });
+
+  stream.pipe(client);
+  client.pipe(stream);
+
+  client.on('end', function() {
+    console.log('client disconnected');
+    stream.end();
+  });
+}
