086fbd7959e92fe2b853b118d2120419de4079d7
codewithpassion <dominik.fretz@gmail.com>
2014-09-08 19:49:10 -0700

    Working HTTP & HTTPS

--- middleware/proxy.js
+++ middleware/proxy.js
@@ -8,81 +8,73 @@ var request = require('request');
 var app = express();
 var server = http.createServer(app);
 var bs = BinaryServer({server: server});
-var net = require('net');
 
 bs.on('connection', function(client){
   console.log('Connection to client');
   // Incoming stream from browsers
   client.on('stream', function(stream, meta) {
-    console.log('meta: ' + meta);
+    var requestData = JSON.parse(meta);
 
-    if (meta.indexOf('{') == 0) {
-
-            var req = JSON.parse(meta);
-      var requestUrl = req.url;
-      console.log('Got SSL connection request to ' + requestUrl);
+    if (requestData.ssl) {
+      var requestUrl = requestData.url;
       var srvUrl = url.parse(requestUrl );
-      console.log(srvUrl.port);
 
       var client = net.connect({port: srvUrl.port, host: srvUrl.hostname},
         function() { //'connect' listener
-          console.log('CONNECTED ' + stream.readable + '/' + stream.writable);
         });
       client.on('connect', function(){
-        console.log('client connected xx');
+        console.log('client connected');
         stream.write('HTTP/1.1 200 Connection Established\r\n' +
           'Proxy-agent: Node-Proxy\r\n' +
           '\r\n');
-        //stream.write("OK\r\n\r\n");
       });
 
       stream.on('data', function (data) {
-        console.log('@@@@@');
         client.write(data);
       });
 
       client.on('data', function(data) {
         stream.write(data);
-        //console.log(data.toString());
-        console.log('####');
       });
 
       client.on('end', function() {
         console.log('client disconnected');
         stream.end();
       });
-
-      return;
     }
+    else {
+      console.log('Stream requested, url: ' + requestData.url);
+      // we first make a HEAD request to see if the file is there. If not, or there
+      // is any other issue, we return the error to the requestor as a JSON object.
+      request.head(requestData.url, function (error, response, body) {
+        if (error || (response !== undefined && response.statusCode >= 400)) {
+          if (response) {
+            console.log(response.statusCode);
+          }
+          var statusCode = 0;
+          if (response) {
+            statusCode = response.statusCode;
+          }
 
-
-/*
-    console.log('Stream requested, url: ' + meta);
-    // we first make a HEAD request to see if the file is there. If not, or there
-    // is any other issue, we return the error to the requestor as a JSON object.
-    request.head(meta, function(error, response, body) {
-      if (error || (response !== undefined && response.statusCode >= 400)) {
-        if (response) { console.log(response.statusCode); }
-        var statusCode = 0;
-        if (response) { statusCode = response.statusCode; }
-
-        stream.write(JSON.stringify({ error: error, statusCode: statusCode}));  
-        stream.end();
-        console.log('There was an error: ' + error + '\nStatus Code: ' + statusCode );
-      }
-      else {
-        // once we are sure all is good, we go ahead and request the file and pipe it to the requestor
-        request(meta, function(error, response, body) {
-          console.log('Done');
+          stream.write(JSON.stringify({ error: error, statusCode: statusCode}));
           stream.end();
-        })
-        // this is where some magic happens.
-        // we pipe the data from the 'request()' directly to the stream
-        // on the browser, this will be piped directly to the ROV.
-        .pipe(stream);
-      }
-    });
- */
+          console.log('There was an error: ' + error + '\nStatus Code: ' + statusCode);
+        }
+        else {
+
+          // once we are sure all is good, we go ahead and request the file and pipe it to the requestor
+          request(requestData.url, function (error, response, body) {
+            console.log('HEAD ' + response.statusCode);
+            console.log('Done');
+            stream.end();
+          })
+            // this is where some magic happens.
+            // we pipe the data from the 'request()' directly to the stream
+            // on the browser, this will be piped directly to the ROV.
+            .pipe(stream);
+        }
+      });
+    }
   });
 
 });
--- proxy-via-browser/index.js
+++ proxy-via-browser/index.js
@@ -23,7 +23,6 @@ app.use(function(req, res){
 });
 
 http.addListener('connect', function(req, socket, head) {
-  console.log('Got CONECT ' + req.url + '\n' + head);
   proxyReq(req, socket, head);
 });
 
@@ -32,30 +31,21 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-// HTTPs
-var key_file   = __dirname + "/certs/key.pem";
-var cert_file  = __dirname + "/certs/cert.pem";
-var config     = {
-  key: fs.readFileSync(key_file),
-  cert: fs.readFileSync(cert_file)
-};
-
-var util = require('util');
-
 // This function acts as a HTTP proxy.
 // The request looks something like: GET HTTP://www.google.com
 // We pass this on to to our proxy on the internet and they download it for us.
 function proxyReq(req, res, head) {
-  //console.log("Request: " + util.inspect(req, false, 1));
+  console.log("Request: " + req.url);
 
   if (currentClient !== null) {
     var ssl = req.method === 'CONNECT';
     var url = (ssl ? 'https://' : '') + req.url;
-    var stream = currentClient.createStream((ssl ? JSON.stringify({ head: head, url: url }) : url) );
+    var stream = currentClient.createStream( JSON.stringify({ head: head, url: url, ssl: ssl }));
     console.log("Created stream for: " + url);
     res.statusCode = 200;
 
     stream.on('data', function(data) {
+      console.log('###');
       // error handling
       if (data[0] == '{' && data[data.length-1] == '}') {
         error = JSON.parse(data);
@@ -69,7 +59,7 @@ function proxyReq(req, res, head) {
     });
 
     req.socket.on('data', function(data) {
-      console.log(stream.write(data));
+      stream.write(data);
     });
 
     // this is where some of the magic happens.
@@ -77,8 +67,8 @@ function proxyReq(req, res, head) {
     // of a file and pipes the response to us.
     // Thanks to JavaScript streams, we can just pipe the data on towards our
     // client.
-    //stream.pipe(res);
-    //req.pipe(stream);
+//    stream.pipe(res);
+//    req.pipe(stream);
 
     stream.on('end', function() {
       console.log("END");
