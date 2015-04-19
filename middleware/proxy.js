4ae92b6edaad6c9d40e3457aa023f625e5257972
BrianAdams <brian@babastik.com>
2015-04-18 17:19:31 -0700

    fixed #397, #391, #365, #362

--- middleware/proxy.js
+++ middleware/proxy.js
@@ -4,81 +4,102 @@ var http = require('http');
 var BinaryServer = require('binaryjs').BinaryServer;
 var express = require('express');
 var request = require('request');
-
 var app = express();
 var server = http.createServer(app);
-var bs = BinaryServer({port: 3011});
+var bs = BinaryServer({ port: 3011 });
 var io = require('socket.io')(server);
-
-bs.on('connection', function(client){
+bs.on('connection', function (client) {
   console.log('Connection to client');
   // Incoming stream from browsers
-  client.on('stream', function(stream, meta) {
+  client.on('stream', function (stream, meta) {
     var requestData = JSON.parse(meta);
-    var handle = requestData.ssl ? handleSsl : handleHttp;
+    var handle;
+    if (requestData.version == 1) {
+      console.log('version=1');
+      handle = requestData.ssl ? handleSsl : handleHttp1;
+    } else {
+      console.log('version=0');
+      handle = requestData.ssl ? handleSsl : handleHttp;
+    }
     handle(requestData, stream);
   });
-
+  //helps keep the stream open over unreliable internet as the clients
+  //don't auto reconnect well yet.
   client.heartbeat = setInterval(function () {
-    var stream = client.send("heartbeat");
-    console.log("<beat>");
+    var stream = client.send('heartbeat');
     stream.end();
-  },5000);
-
-  client.on('end', function(){
+  }, 5000);
+  client.on('end', function () {
     clearInterval(client.heartbeat);
-  })
+  });
 });
-server.listen(3001, function() {
+server.listen(3001, function () {
   console.log('HTTP and BinaryJS server started on port 3001');
 });
-
+function handleHttp1(requestData, stream) {
+  console.log('Stream requested, url: ' + requestData.url);
+  // we first make a HEAD request to get access to the headers before
+  // streaming the rest of the url data.
+  request.head(requestData.url, function (error, response, body) {
+    if (error) {
+      stream.write('HTTP/1.1 ' + 500 + ' ' + 'Proxy Service Error' + '\r\n');
+      stream.end();
+      console.log('There was an error: ');
+      console.dir(error);
+      return;
+    }
+    stream.write('HTTP/1.1 ' + response.statusCode + ' ' + response.statusMessage + '\r\n');
+    for (var item in response.headers) {
+      stream.write(item + ': ' + response.headers[item] + '\r\n');
+    }
+    stream.write('\r\n');
+    // once we are sure all is good, we go ahead and request the file and pipe it to the requestor
+    request(requestData.url, function (error, response, body) {
+      console.log('Done');
+      stream.end();
+    }).pipe(stream);
+  });
+}
+//Depricated  - for backwards compatibility with earlier clients. Remove
+//after 2015-Oct.
 function handleHttp(requestData, stream) {
   console.log('Stream requested, url: ' + requestData.url);
   // we first make a HEAD request to see if the file is there. If not, or there
   // is any other issue, we return the error to the requestor as a JSON object.
   request.head(requestData.url, function (error, response, body) {
-    if (error || (response !== undefined && response.statusCode >= 400)) {
+    if (error || response !== undefined && response.statusCode >= 400) {
       var statusCode = 0;
       if (response) {
         statusCode = response.statusCode;
       }
-      stream.write(JSON.stringify({ error: error, statusCode: statusCode}));
+      stream.write(JSON.stringify({
+        error: error,
+        statusCode: statusCode
+      }));
       stream.end();
       console.log('There was an error: ' + error + '\nStatus Code: ' + statusCode);
-    }
-    else {
-
+    } else {
       // once we are sure all is good, we go ahead and request the file and pipe it to the requestor
       request(requestData.url, function (error, response, body) {
         console.log('Done');
         stream.end();
-      })
-        // this is where some magic happens.
-        // we pipe the data from the 'request()' directly to the stream
-        // on the browser, this will be piped directly to the ROV.
-        .pipe(stream);
+      }).pipe(stream);
     }
   });
 }
-
 function handleSsl(requestData, stream) {
   var requestUrl = requestData.url;
-  var srvUrl = url.parse(requestUrl );
-
-  var client = net.connect(
-    {port: srvUrl.port, host: srvUrl.hostname},
-    function() {
-      stream.write('HTTP/1.1 200 Connection Established\r\n' +
-        'Proxy-agent: Node-Proxy\r\n' +
-        '\r\n');
+  var srvUrl = url.parse(requestUrl);
+  var thestream = stream;
+  var client = net.connect({
+      port: srvUrl.port,
+      host: srvUrl.hostname
+    }, function () {
+      stream.write('HTTP/1.1 200 Connection Established\r\n' + 'Proxy-agent: Node-Proxy\r\n' + '\r\n');
+      thestream.pipe(client);
+      client.pipe(thestream);
     });
-
-  stream.pipe(client);
-  client.pipe(stream);
-
-  client.on('end', function() {
-    console.log('client disconnected');
+  client.on('end', function () {
     stream.end();
   });
 }
--- proxy-via-browser/index.js
+++ proxy-via-browser/index.js
@@ -1,79 +1,101 @@
 var fs = require('fs');
-var app = require('express')();
-var http = require('http').Server(app);
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
 var BinaryServer = require('binaryjs').BinaryServer;
-var bs = BinaryServer({port: 3010});
-var io = require('socket.io')(http);
-
+var bs = BinaryServer({ port: 3010 });
 var currentClient = null;
-
+var request = 0;
 io.on('connection', function (socket) {
   console.log('Socket.IO: connected');
 });
-
-bs.on('connection', function(client) {
+bs.on('connection', function (client) {
   console.log('Connection to client');
   // this could be improved as right now we only keep one client (browser).
   // Should not be an issue as we only should have one browser connected anyway.
   currentClient = client;
 });
-
-app.use(function(req, res){
-  if (req.url == '/' ) { // if the request is for '/' we send the index file
-    res.sendfile(__dirname + '/public/index.html');
-  }
-  // otherwise we proxying the request
-  else { proxyReq(req, res); }
-});
-
-http.addListener('connect', function(req, socket, head) {
+app.addListener('connect', function (req, socket, head) {
   proxyReq(req, socket, head);
 });
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
+function handler(req, res) {
+  if (req.url == '/') {
+    // if the request is for '/' we send the index file
+    fs.readFile(__dirname + '/public/index.html', function (err, data) {
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': data.length
+      });
+      res.write(data);
+      res.end();
+    });
+  }  // otherwise we proxying the request
+  else {
+    proxyReq(req, res);
+  }
+}
+app.listen(3000);
+console.log('Server is listening');
 // This function acts as a HTTP proxy.
 // The request looks something like: GET HTTP://www.google.com
 // We pass this on to to our proxy on the internet and they download it for us.
 function proxyReq(req, res, head) {
-  console.log("Request: " + req.url);
-
+  console.log('Request: ' + req.url);
   if (currentClient !== null) {
     var ssl = req.method === 'CONNECT';
     var url = (ssl ? 'https://' : '') + req.url;
-    var stream = currentClient.createStream( JSON.stringify({ head: head, url: url, ssl: ssl }) );
-    console.log("Created stream for: " + url);
-    res.statusCode = 200;
-
-    stream.on('data', function(data) {
-      // error handling
-      if (data[0] == '{' && data[data.length-1] == '}') {
-        error = JSON.parse(data);
-        console.log("Error occurd " + JSON.stringify(error.error) + ' ' + error.statusCode);
-        res.statusCode = error.statusCode === 0 ? 500 : error.statusCode;
-        res.end("Error");
+    var streamid = request;
+    request++;
+    var stream = currentClient.createStream(JSON.stringify({
+        version: 1,
+        head: head,
+        url: url,
+        ssl: ssl
+      }));
+    console.log('Created stream for: ' + url);
+    var headers_set = false;
+    var response_parsed = false;
+    for (var h in res.headers) {
+      res.removeHeader(h);
+    }
+    if (ssl) {
+      headers_set = true;
+      response_parsed = true;
+      stream.pipe(res);
+      req.socket.pipe(stream);
+    }
+    stream.on('data', function (data) {
+      if (!headers_set) {
+        if (data == '\r\n' || data == '\n') {
+          headers_set = true;
+          // this is where some of the magic happens.
+          // We sent the url to the proxy and the proxy starts a download
+          // of a file and pipes the response to us.
+          // Thanks to JavaScript streams, we can just pipe the data on towards our
+          // client.
+          stream.pipe(res);
+          req.socket.pipe(stream);
+        } else {
+          var headerparts = data.split(/:(.+)?/);
+          if (response_parsed) {
+            headerparts[0] = headerparts[0].charAt(0).toUpperCase() + headerparts[0].slice(1);
+            res.setHeader(headerparts[0], headerparts[1]);
+            console.log('Set Header: ' + data);
+          } else {
+            res.statusCode = data.split(' ')[1];
+            res.statusMessage = data.split(' ')[2];
+            response_parsed = true;
+          }
+        }
       }
     });
-
-    // this is where some of the magic happens.
-    // We sent the url to the proxy and the proxy starts a download
-    // of a file and pipes the response to us.
-    // Thanks to JavaScript streams, we can just pipe the data on towards our
-    // client.
-    stream.pipe(res);
-    req.socket.pipe(stream);
-
-    stream.on('end', function() {
+    stream.on('end', function () {
       // When the proxy tells us everything was sent, we end the response to the client.
       res.end();
     });
-  }
-  else {
+  } else {
     console.log('No client connected!');
     res.statusCode = 500;
-    res.end("No client connected!");
+    res.end('No client connected!');
   }
 }
